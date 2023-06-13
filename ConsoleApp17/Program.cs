1aa35b0f35386f623249395d221fbb092b9d42ef
BramvanVeen <135881925+BramvanVeen@users.noreply.github.com>
2023-06-13 13:25:05 +0200

    Update Program.cs

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp17
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("GIVEMESOMEGITHUB");
         }
     }
 }
